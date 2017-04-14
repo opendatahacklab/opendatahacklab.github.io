59c360f5617b78060cdf6b407613e1d9b3251ecb
cristianolongo <cristianolongo@gmail.com>
2017-04-14 18:39:52 +0200

    #96 use semantic octopus for datasets

--- uploadOnt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-java -jar ../semanticoctopus/target/semanticoctopus-0.1.1.jar http://localhost/~cristianolongo/opendatahacklab/site/odhl.owl >odhlfull.owl
-curl -X PUT -H "Content-Type: application/rdf+xml" -u 'cristianolongo'  --data-binary @odhlfull.owl http://dydra.com/cristianolongo/odhl/service
-sleep 5
-rm odhlfull.owl
-
