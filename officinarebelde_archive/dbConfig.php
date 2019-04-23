6dd0035326f5c3fa1e997588577018a34ed0a30f
Cristiano Longo <cristianolongo@hackspacecatania.it>
2019-04-23 15:59:31 +0200

    officina rebelde articles list

---
+++ officinarebelde_archive/dbConfig.php
@@ -0,0 +1,7 @@
+<?php
+	$host="127.0.0.1";
+	$database="Sql165907_1";
+	$username="Sql165907";
+	$password="6b024247";
+	$password2="cP{q]?N?(@-J";
+?>
---
+++ officinarebelde_archive/listArticles.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="it">
+	<head>
+		<meta charset="cp-1252" />
+		<title>Archivio di www.officinarebelde.org</title>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<th />
+				<th>Titolo</th>
+				<th>Data di Creazione</th>
+				<th>Data di Modifica</th>
+			</tr>
+<?php
+	require("dbConfig.php");
+	$db = new mysqli($host,$username, $password,$database) or die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error); 
+	$res=$db->query("SELECT id_article, titre, date, date_modif FROM spip_articles order by id_article");
+	echo "found $res->num_rows articles\n";
+	foreach($res as $a){
+		$id=$a['id_article'];
+		$t=$a['titre'];
+		$c=$a['date'];
+		$m=$a['date_modif'];
+		echo "\t\t\t<tr><td>$id</td><td>$t</td><td>$c</td><td>$m</td></tr>\n";
+	}
+	$db->close();
+?>
+	</body>
+</html>
